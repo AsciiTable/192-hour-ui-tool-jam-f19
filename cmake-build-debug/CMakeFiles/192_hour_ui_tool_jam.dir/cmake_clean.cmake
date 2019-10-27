a8e4e86b343a696baf7712cf43c665f4f6557b60
Jessica Wei <jessicawei9@gmail.com>
2019-10-26 20:18:31 -0700

    initialize project

---
+++ cmake-build-debug/CMakeFiles/192_hour_ui_tool_jam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/192_hour_ui_tool_jam.dir/main.cpp.o"
+  "192_hour_ui_tool_jam.pdb"
+  "192_hour_ui_tool_jam"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/192_hour_ui_tool_jam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
